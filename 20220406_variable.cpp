ac697a3214e796e25969f6b54d5927a0aa03e521
Hernicksen <103085874+Hernicksen@users.noreply.github.com>
2022-04-06 11:09:08 +0800

    Create 20220406_variable.cpp

---
+++ 20220406_variable.cpp
@@ -0,0 +1,16 @@
+//header
+#include <iostream>
+
+using namespace std;
+int main() {
+
+    float angka1, angka2;
+    string nama;
+    
+    nama = "Hernicksen";
+    angka1 = 10.21;
+    angka2 = 3.14;
+    
+    cout<<nama<<endl;
+    cout<<angka1+angka2;
+}
